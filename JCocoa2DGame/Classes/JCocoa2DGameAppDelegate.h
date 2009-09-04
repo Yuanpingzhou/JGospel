edb9987053de80dfd3d3356758fc1c57a367b1b5
Yuanpingzhou <yuanpingzhou0624@gmail.com>
2009-09-04 15:23:37 +0800

    V1.0 JCocoa2DGame for JGospel

---
+++ JCocoa2DGame/Classes/JCocoa2DGameAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  JCocoa2DGameAppDelegate.h
+//  JCocoa2DGame
+//
+//  Created by user on 9/4/09.
+//  Copyright Taihootech 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCocoa2DGameAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
